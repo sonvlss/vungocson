5c0f0b50da2115a8a894afebbed36c361477be2d
VuNgocSon <vungocson1863@gmail.com>
2024-05-15 10:25:46 +0700

    JavaApplication3

---
+++ src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author Admin
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+class HelloWorldApp{ 
+                  public static void main(String[] args){
+	              //In dong chu “HelloWorld” 	  
+                        System.out.println(“HelloWorld”); 
+                  } 
+           } 
